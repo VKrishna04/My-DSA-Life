a55654eb706414e7b1bf0c47ccd8538905d7f80d
Krishna GSVV <VKrishna04@users.noreply.github.com>
2024-10-21 18:53:27 +0000

    [Math] Find the Key of the Numbers solved

---
+++ problems/lc-find-the-key-of-the-numbers::1429658026/lc-find-the-key-of-the-numbers::1429658026.py
@@ -0,0 +1,6 @@
+class Solution:
+    def generateKey(self, num1: int, num2: int, num3: int) -> str:
+        # nums = [str(num).zfill(4) for num in [num1, num2, num3]]
+        # key = ''.join(min(num[i] for num in nums) for i in range(4))
+        # return int(key)
+        return int(''.join(min(num[i] for num in [str(num).zfill(4) for num in [num1, num2, num3]]) for i in range(4)))
